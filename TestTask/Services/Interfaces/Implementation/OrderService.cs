740d15a0db31eb7089c330e8d14d352ab8adb580
solfire1994 <solfire1994@gmail.com>
2023-10-08 19:03:59 +0300

    Add project files.

---
+++ TestTask/Services/Interfaces/Implementation/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces.Implementation
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext Context;
+        public OrderService (ApplicationDbContext context)
+        {
+            Context = context;
+        }
+        public Task<Order> GetOrder()
+        {
+            Order result = Context.Orders.FirstOrDefault();
+            foreach (var item in Context.Orders)
+            {
+                result = item.Price < result.Price ? result : item;
+            }
+            return Task.FromResult(result);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            List<Order> result = new();
+
+            foreach (var item in Context.Orders)
+            {
+                if(item.Quantity > 10) result.Add(item);
+            }
+            return Task.FromResult(result);
+        }
+    }
+}
